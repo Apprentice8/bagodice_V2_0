c242ee934e1932080bd028ce1193e20baf18ccee
Calvin <fullstack@calvinhdesign.com>
2015-07-08 12:52:08 -0700

    pushing dice changes to git

--- JS/dice.js
+++ JS/dice.js
@@ -2,14 +2,22 @@ function rollDice () {
 	var dice = document.getElementById("diceSelector").value;
 	var multiplier = document.getElementById("diceMultiplier").value;
 	
-	//generate random value between 0 and value on dice for each dice rolled
+	//generate dice values
 	var diceValue = new Array();	
 	for (var i=1; i <= multiplier; i++) {
 		function randomNumber() {
-		diceValue.push(this["diceValue"+i] = Math.floor(Math.random() * (dice -1) + 1));
+			//if percentile dice is rolled, incraments of 10 from 0-90
+			if (dice === "100") { 
+				diceValue.push(this["diceValue"+i] = Math.floor(Math.random() * 10)*10); 
+			}
+			//if any other die, generate any random number between 1 and max value of dice
+			else {
+				diceValue.push(this["diceValue"+i] = Math.floor((Math.random() * dice) + 1));
+			}
 		}
+		//roll dice
 		randomNumber();
-		console.log(diceValue);
 	}
-
+//print values to console
+console.log(diceValue);
 }
