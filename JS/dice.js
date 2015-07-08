431c2549a27014ed345d1c8378e90b25f96816c1
Calvin <fullstack@calvinhdesign.com>
2015-07-08 15:38:47 -0700

    pushing dice changes to git

--- JS/dice.js
+++ JS/dice.js
@@ -1,15 +1,30 @@
+//this function checks if user input is a valid number and throws some helpful error text if not. 
 function validEntry() {
 	var validMultiplier = document.getElementById("diceMultiplier").value;
+	var switchClass = document.getElementById("diceMultiplier");
+	var multAlert = document.getElementById("multiplierAlert");
+	
+	//is the user trying to input a letter or special character? if so, stop it!
 	if (isNaN(validMultiplier)) {
-		alert("No letters or special characters are permitted. Please enter only numbers (i.e. 0-9)");
+		switchClass.className = "alertClass";
+		multAlert.innerHTML = "Please enter only numbers (i.e. 0-9)<br />";
 	}
-	else if (validMultiplier == "") {
-		alert("Multiplier must not be blank. Please enter the number of dice you would like to roll.")	
+	//has the user left the multiplier field blank? 
+	else if (validMultiplier.trim() === "") {
+		switchClass.className = "alertClass";
+		multAlert.innerHTML = "Field cannot be blank.<br />";		
+	}
+	//has the user has input a valid number? Keep calm and move along. 
+	else {
+		switchClass.className = "noClass";
+		multAlert.innerHTML = "";	
 	}
 }
 
+//this function is the meat and potatoes of the "Bag O' Dice." This is where the magic happens
 function rollDice () {
 	
+	//first, check if the user input is kosher
 	validEntry();
 	
 	//clear previous dice values
@@ -26,7 +41,7 @@ function rollDice () {
 		function randomNumber() {			
 			//if d100 is rolled, randomly generate number from 0-90 in incraments of 10
 			if (dice === "100") { 
-				diceValue.push(this["diceValue"+i] = Math.floor(Math.random() * 10)*10); 
+				diceValue.push(this["diceValue"+i] = Math.floor(Math.random() * 10)*10);
 			}
 			//if any other die, generate a random number between 1 and max value of dice
 			else {
@@ -37,11 +52,11 @@ function rollDice () {
 		randomNumber();
 	}
 	//print values to user
-	document.getElementById("dice_output").innerHTML += (diceValue);
+	document.getElementById("dice_output").innerHTML += ("<h3>Roll Values</h3><p>" + diceValue + "</p>");
 	//total dice
 	var total = 0;
 		for (var j = 0; j < diceValue.length; j++) {
 			total += diceValue[j] << 0;
 		}
-	document.getElementById("dice_total").innerHTML += (total);
+	document.getElementById("dice_total").innerHTML += ("<h3>Dice Total</h3><p>= " + total + "</p>");
 }
